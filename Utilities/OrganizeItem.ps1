61cb2169f974ee8067aa97bf9425fc7543db2172
Lokesh Balu <lokeshkumarbalu@gmail.com>
2016-12-06 23:35:12 +0100

    Rename Script.ps1 to OrganizeItem.ps1

---
+++ Utilities/OrganizeItem.ps1
@@ -0,0 +1,22 @@
+﻿#requires -version 2
+<#
+.SYNOPSIS
+  Script to organize files in a directory.
+.DESCRIPTION
+  This script helps the user to better organize file or items in a directory.
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+.NOTES
+  Version:        1.0
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
