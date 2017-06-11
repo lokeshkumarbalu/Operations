45468ef78777190f33bb8f4385262646c60d1aa6
Lokesh Balu <lokeshkumarbalu@gmail.com>
2017-06-12 00:54:06 +0200

    Add NewVolumeFile.ps1

---
+++ Utilities/NewVolumeFile.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+  Script to generate CSV gas volume file for upload.
+
+.DESCRIPTION
+  This script generates a CSV volume file for upload, the structure of the CSV is predefined.
+
+  Gas Volume file structure:
+  ==> <Deal number>; <Start date>; <End date>; <Volume type>; <Volume>
+
+.NOTES
+  Author: Lokesh Kumar Balu
+  Date: June 12, 2017
+  Copyright © Zealag 2017. All rights reserved.
+#>
+
+#Requires -Version 3.0
